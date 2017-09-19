98b9b9912136e65c5a858c13d2efaa7892dc30f0
Stanislav Abramov <abramov.center@yandex.ru>
2017-09-19 16:48:50 +0000

    task0

---
+++ task0/sort.h
@@ -0,0 +1,12 @@
+#ifndef TASK0_SORT_H
+#define TASK0_SORT_H
+#include <string>
+#include <list>
+using namespace std;
+
+namespace sort {
+    void sort_strings(list<string> &listing);
+}
+
+#endif //TASK0_SORT_H
+
