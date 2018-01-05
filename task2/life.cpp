48b9487bc83aebe5e06a2d468651c0bfdeb34cd4
Stanislav Abramov <abramov.center@yandex.ru>
2018-01-05 15:36:02 +0000

    life.cpp created online with Bitbucket

---
+++ task2/life.cpp
@@ -0,0 +1,23 @@
+#include "game.hpp"
+#include "parserAuto.hpp"
+#include "parserInterective.hpp"
+
+using namespace life;
+
+int main(int argc, char** argv)
+{
+std::cout << "fffffff";
+	if (argc > 1){
+		
+	 	AutoMode hello(argv,argc);
+		//  if (hello.validAuto() == -1)
+		// 	return 0;
+
+
+	}
+
+
+
+	return 0;
+}
+
